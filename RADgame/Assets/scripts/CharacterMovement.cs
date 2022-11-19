0d56ad53c5060d554b4e2a84da796f3e978cd538
YunisUA <yunis.barudi@students.ittralee.ie>
2022-11-19 15:34:40 +0000

    Signed-off-by: YunisUA <yunis.barudi@students.ittralee.ie>

---
+++ RADgame/Assets/scripts/CharacterMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float movementSpeed = 6f;
+    public float jumpHigh = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Take this from Edit - Project Settings - Input Manager
+        float horizontalInnput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalInnput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpHigh, rb.velocity.z);
+        }
+     
+       
+    }
+}
